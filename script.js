aa53f6431282acb21be444cf3ec0f75de12c2181
Stanislav Shkradov <studingdev@gmail.com>
2023-04-30 13:57:32 +0300

    init:start keyboard-task

---
+++ script.js
@@ -0,0 +1,171 @@
+// let arrOfEngLetters = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+// console.log(arrOfEngLetters.length)
+
+// let objOfEngLetters2 = {
+//     "Backquote": "`",
+//     "Digit1": "1",
+//     "Digit2": "2",
+//     "Digit3": "3",
+//     "Digit4": "4",
+//     "Digit5": "5",
+//     "Digit6": "6",
+//     "Digit7": "7",
+//     "Digit8": "8",
+//     "Digit9": "9",
+//     "Digit0": "0",
+//     "Minus": "-",
+//     "Equal": "=",
+//     "Backspace": "Backspace",
+//     "Tab": "Tab",
+//     "KeyQ": "q",
+//     "KeyW": "w",
+//     "KeyE": "e",
+//     "KeyR": "r",
+//     "KeyT": "t",
+//     "KeyY": "y",
+//     "KeyU": "u",
+//     "KeyI": "i",
+//     "KeyO": "o",
+//     "KeyP": "p",
+//     "BracketLeft": "[",
+//     "BracketRight": "]",
+//     "Backslash": "\\",
+//     "CapsLock": "CapsLock",
+//     "KeyA": "A",
+//     "KeyS": "S",
+//     "KeyD": "D",
+//     "KeyF": "F",
+//     "KeyG": "G",
+//     "KeyH": "H",
+//     "KeyJ": "J",
+//     "KeyK": "K",
+//     "KeyL": "L",
+//     "Semicolon": ";",
+//     "Quote": "'",
+//     "Enter": "Enter",
+//     "ShiftLeft": "Shift",
+//     "KeyZ": "Z",
+//     "KeyX": "X",
+//     "KeyC": "C",
+//     "KeyV": "V",
+//     "KeyB": "B",
+//     "KeyN": "N",
+//     "KeyM": "M",
+//     "Comma": ",",
+//     "Period": ".",
+//     "Slash": "/",
+//     "ArrowUp": "ArrowUp",
+//     "ShiftRight": "Shift",
+//     "ControlLeft": "Control",
+//     "MetaLeft": "Win",
+//     "AltLeft": "Alt",
+//     "Space": " ",
+//     "AltRight": "Alt",
+//     "ControlRight": "Control",
+//     "ArrowLeft": "ArrowLeft",
+//     "ArrowDown": "ArrowDown",
+//     "ArrowRight": "ArrowRight"
+// }
+// console.log(Object.keys(objOfEngLetters2).length)
+
+let keyboard = document.querySelector(`#keyboard`);
+
+let rowsOfButtons = [
+    {
+    "Backquote": "`",
+    "Digit1": "1",
+    "Digit2": "2",
+    "Digit3": "3",
+    "Digit4": "4",
+    "Digit5": "5",
+    "Digit6": "6",
+    "Digit7": "7",
+    "Digit8": "8",
+    "Digit9": "9",
+    "Digit0": "0",
+    "Minus": "-",
+    "Equal": "=",
+    "Backspace": "Backspace"
+    },
+    {
+    "Tab": "Tab",
+    "KeyQ": "q",
+    "KeyW": "w",
+    "KeyE": "e",
+    "KeyR": "r",
+    "KeyT": "t",
+    "KeyY": "y",
+    "KeyU": "u",
+    "KeyI": "i",
+    "KeyO": "o",
+    "KeyP": "p",
+    "BracketLeft": "[",
+    "BracketRight": "]",
+    "Backslash": "\\"
+    },
+    {
+    "CapsLock": "CapsLock",
+    "KeyA": "A",
+    "KeyS": "S",
+    "KeyD": "D",
+    "KeyF": "F",
+    "KeyG": "G",
+    "KeyH": "H",
+    "KeyJ": "J",
+    "KeyK": "K",
+    "KeyL": "L",
+    "Semicolon": ";",
+    "Quote": "'",
+    "Enter": "Enter"
+    },
+    {
+    "ShiftLeft": "Shift",
+    "KeyZ": "Z",
+    "KeyX": "X",
+    "KeyC": "C",
+    "KeyV": "V",
+    "KeyB": "B",
+    "KeyN": "N",
+    "KeyM": "M",
+    "Comma": ",",
+    "Period": ".",
+    "Slash": "/",
+    "ArrowUp": "ArrowUp",
+    "ShiftRight": "Shift"
+    },
+    {
+    "ControlLeft": "Control",
+    "MetaLeft": "Win",
+    "AltLeft": "Alt",
+    "Space": " ",
+    "AltRight": "Alt",
+    "ControlRight": "Control",
+    "ArrowLeft": "ArrowLeft",
+    "ArrowDown": "ArrowDown",
+    "ArrowRight": "ArrowRight"
+    }
+]
+
+for (let element of rowsOfButtons) {
+    let buttonRow = document.createElement(`div`);
+    buttonRow.classList.add(`buttonRow`);
+    keyboard.append(buttonRow)
+    for (let el in element) {
+        let button = document.createElement(`div`);
+        button.classList.add(`button`);
+        button.classList.add(el);
+        button.innerHTML = element[el];
+        buttonRow.append(button)
+    }
+}
+
+
+
+
+document.onkeydown = function(e)
+{
+
+    // objOfEngLetters2[e.code] = e.key
+
+    console.log(e.code, e.key)
+}
