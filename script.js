bbb460ff230c4d49c9d7195befeb6bf0c19d33a5
Stanislav Shkradov <studingdev@gmail.com>
2023-04-30 22:49:31 +0300

    feat: add animation, input of text

--- script.js
+++ script.js
@@ -128,21 +128,96 @@ let rowsOfButtons = [
     "Comma": ",",
     "Period": ".",
     "Slash": "/",
-    "ArrowUp": "ArrowUp",
+    "ArrowUp": "▲",
     "ShiftRight": "Shift"
     },
     {
-    "ControlLeft": "Control",
+    "ControlLeft": "Ctrl",
     "MetaLeft": "Win",
     "AltLeft": "Alt",
     "Space": " ",
     "AltRight": "Alt",
-    "ControlRight": "Control",
-    "ArrowLeft": "ArrowLeft",
-    "ArrowDown": "ArrowDown",
-    "ArrowRight": "ArrowRight"
+    "ArrowLeft": "◄",
+    "ArrowDown": "▼",
+    "ArrowRight": "►",
+    "ControlRight": "Ctrl"
     }
 ]
+
+let russianButtons = {
+    "Backquote": "ё",
+    "KeyQ": "й",
+    "KeyW": "ц",
+    "KeyE": "у",
+    "KeyR": "к",
+    "KeyT": "е",
+    "KeyY": "н",
+    "KeyU": "г",
+    "KeyI": "ш",
+    "KeyO": "щ",
+    "KeyP": "з",
+    "BracketLeft": "х",
+    "BracketRight": "ъ",
+    "KeyA": "ф",
+    "KeyS": "ы",
+    "KeyD": "в",
+    "KeyF": "а",
+    "KeyG": "п",
+    "KeyH": "р",
+    "KeyJ": "о",
+    "KeyK": "л",
+    "KeyL": "д",
+    "Semicolon": "ж",
+    "Quote": "э",
+    "KeyZ": "я",
+    "KeyX": "ч",
+    "KeyC": "с",
+    "KeyV": "м",
+    "KeyB": "и",
+    "KeyN": "т",
+    "KeyM": "ь",
+    "Comma": "б",
+    "Period": "ю",
+    "Slash": "."
+};
+
+let engButtons = {
+    "Backquote": "`",
+    "KeyQ": "q",
+    "KeyW": "w",
+    "KeyE": "e",
+    "KeyR": "r",
+    "KeyT": "t",
+    "KeyY": "y",
+    "KeyU": "u",
+    "KeyI": "i",
+    "KeyO": "o",
+    "KeyP": "p",
+    "BracketLeft": "[",
+    "BracketRight": "]",
+    "KeyA": "a",
+    "KeyS": "s",
+    "KeyD": "d",
+    "KeyF": "f",
+    "KeyG": "g",
+    "KeyH": "h",
+    "KeyJ": "j",
+    "KeyK": "k",
+    "KeyL": "l",
+    "Semicolon": ";",
+    "Quote": "'",
+    "KeyZ": "z",
+    "KeyX": "x",
+    "KeyC": "c",
+    "KeyV": "v",
+    "KeyB": "b",
+    "KeyN": "n",
+    "KeyM": "m",
+    "Comma": ",",
+    "Period": ".",
+    "Slash": "/",
+};
+
 let body = document.querySelector(`body`);
 
 let innerField = document.createElement(`textarea`);
@@ -159,21 +234,97 @@ for (let element of rowsOfButtons) {
     buttonRow.classList.add(`buttonRow`);
     keyBoard.append(buttonRow)
     for (let el in element) {
-        let button = document.createElement(`div`);
+        var button = document.createElement(`div`);
         button.classList.add(`button`);
         button.classList.add(el);
         button.innerHTML = element[el];
-        buttonRow.append(button)
+        buttonRow.append(button);
+
     }
 }
 
+// document.addEventListener ( `keydown`, function func (e){
+//     for (let element of keyBoard.children) {
+//         for (let el of element.children) {
+//             if ([...el.classList].includes(e.code)) {
+//                 el.style.backgroundColor = `green`;
+//             }
+//         }
+//     }
+// }
+// )
+
+// document.addEventListener(`keydown`, function (e) {
+//     console.log(e.code, e.key)
+// russianButtons[e.code] = e.key
+// console.log(russianButtons)
+// })
+
+let turner = [];
+document.addEventListener(`keydown`, function colorChangerOn (e){
+    
+    for (let element of keyBoard.children) {
+        for (let el of element.children) {
+            if ([...el.classList].includes(e.code)) {
+                el.style.transform = `scale(0.9)`;
+                el.style.boxShadow = `0px 10px 20px 2px rgba(0, 0, 0, 0.25)`;
+            }
+        }
+    }
+
+    // Меняет раскладку
+    
+    if ((e.code === 'ShiftLeft' || e.code === 'AltLeft') && turner.length < 2) {
+        turner.push (e.code)
+    } else if (turner.length >= 2) {
+        turner.splice(0, 1);
+    }
+    if (turner[0] === 'ShiftLeft' && turner[1] === 'AltLeft')  {
+        for (let element of keyBoard.children) {
+            for (let i = 0; i < element.children.length; i++) { 
+                if (element.children[i].innerHTML != engButtons[element.children[i].classList[1]] && Object.keys(engButtons).includes(element.children[i].classList[1])) {
+                    console.log(engButtons[element.children[i].classList[1]]);
+                    element.children[i].innerHTML = engButtons[element.children[i].classList[1]];
+                } else if (element.children[i].innerHTML != russianButtons[element.children[i].classList[1]] && Object.keys(russianButtons).includes(element.children[i].classList[1])) {
+                    element.children[i].innerHTML = russianButtons[element.children[i].classList[1]];
+                }
+            }
+        }
+
+        console.log(turner)
+        turner = [];
+        
+    }
+    
+    // Меняет регистр
+
+    if (e.code === 'CapsLock') {
+        for (let element of keyBoard.children) {
+            for (let el of element.children) {
+                if (el.innerHTML.length <= 1 && el.innerHTML[0] != el.innerHTML[0].toUpperCase()) {
+                    el.innerHTML = el.innerHTML.toUpperCase();
+                } else if (el.innerHTML.length <= 1 && el.innerHTML[0] == el.innerHTML[0].toUpperCase()){
+                    el.innerHTML = el.innerHTML.toLowerCase();
+                }
+            }
+        }
+    }
+    
+})
 
 
-
-document.onkeydown = function(e)
-{
-
-    // objOfEngLetters2[e.code] = e.key
-
-    console.log(e.code, e.key)
+document.onkeyup = function colorChangerOff (e){
+    for (let element of keyBoard.children) {
+        for (let el of element.children) {
+            if ([...el.classList].includes(e.code)) {
+                el.style.transform = `scale(1)`;
+                el.style.boxShadow = `2px 2px 2px 0px rgba(0,0,0,0.3)`;
+                if (el.innerHTML.length <= 1) {
+                    innerField.innerHTML += el.innerHTML
+                }
+            }
+            
+        }
+    }
+    
 }
