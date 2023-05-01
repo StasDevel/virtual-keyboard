9a12367b85a13f7349e4f8294e9e6f304de3933f
Stanislav Shkradov <studingdev@gmail.com>
2023-05-01 14:41:24 +0300

    fix: fixed language switcher

--- script.js
+++ script.js
@@ -1,72 +1,8 @@
-// let arrOfEngLetters = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
-// console.log(arrOfEngLetters.length)
-
-// let objOfEngLetters2 = {
-//     "Backquote": "`",
-//     "Digit1": "1",
-//     "Digit2": "2",
-//     "Digit3": "3",
-//     "Digit4": "4",
-//     "Digit5": "5",
-//     "Digit6": "6",
-//     "Digit7": "7",
-//     "Digit8": "8",
-//     "Digit9": "9",
-//     "Digit0": "0",
-//     "Minus": "-",
-//     "Equal": "=",
-//     "Backspace": "Backspace",
-//     "Tab": "Tab",
-//     "KeyQ": "q",
-//     "KeyW": "w",
-//     "KeyE": "e",
-//     "KeyR": "r",
-//     "KeyT": "t",
-//     "KeyY": "y",
-//     "KeyU": "u",
-//     "KeyI": "i",
-//     "KeyO": "o",
-//     "KeyP": "p",
-//     "BracketLeft": "[",
-//     "BracketRight": "]",
-//     "Backslash": "\\",
-//     "CapsLock": "CapsLock",
-//     "KeyA": "A",
-//     "KeyS": "S",
-//     "KeyD": "D",
-//     "KeyF": "F",
-//     "KeyG": "G",
-//     "KeyH": "H",
-//     "KeyJ": "J",
-//     "KeyK": "K",
-//     "KeyL": "L",
-//     "Semicolon": ";",
-//     "Quote": "'",
-//     "Enter": "Enter",
-//     "ShiftLeft": "Shift",
-//     "KeyZ": "Z",
-//     "KeyX": "X",
-//     "KeyC": "C",
-//     "KeyV": "V",
-//     "KeyB": "B",
-//     "KeyN": "N",
-//     "KeyM": "M",
-//     "Comma": ",",
-//     "Period": ".",
-//     "Slash": "/",
-//     "ArrowUp": "ArrowUp",
-//     "ShiftRight": "Shift",
-//     "ControlLeft": "Control",
-//     "MetaLeft": "Win",
-//     "AltLeft": "Alt",
-//     "Space": " ",
-//     "AltRight": "Alt",
-//     "ControlRight": "Control",
-//     "ArrowLeft": "ArrowLeft",
-//     "ArrowDown": "ArrowDown",
-//     "ArrowRight": "ArrowRight"
-// }
-// console.log(Object.keys(objOfEngLetters2).length)
+let arr;
+console.log(arr);
+arr = []
+arr.push(1);
+console.log(arr);
 
 let rowsOfButtons = [
     {
@@ -145,45 +81,22 @@ let rowsOfButtons = [
     }
 ]
 
-let russianButtons = {
-    "Backquote": "ё",
-    "KeyQ": "й",
-    "KeyW": "ц",
-    "KeyE": "у",
-    "KeyR": "к",
-    "KeyT": "е",
-    "KeyY": "н",
-    "KeyU": "г",
-    "KeyI": "ш",
-    "KeyO": "щ",
-    "KeyP": "з",
-    "BracketLeft": "х",
-    "BracketRight": "ъ",
-    "KeyA": "ф",
-    "KeyS": "ы",
-    "KeyD": "в",
-    "KeyF": "а",
-    "KeyG": "п",
-    "KeyH": "р",
-    "KeyJ": "о",
-    "KeyK": "л",
-    "KeyL": "д",
-    "Semicolon": "ж",
-    "Quote": "э",
-    "KeyZ": "я",
-    "KeyX": "ч",
-    "KeyC": "с",
-    "KeyV": "м",
-    "KeyB": "и",
-    "KeyN": "т",
-    "KeyM": "ь",
-    "Comma": "б",
-    "Period": "ю",
-    "Slash": "."
-};
-
-let engButtons = {
+let rowsOfButtonsEng = {
     "Backquote": "`",
+    "Digit1": "1",
+    "Digit2": "2",
+    "Digit3": "3",
+    "Digit4": "4",
+    "Digit5": "5",
+    "Digit6": "6",
+    "Digit7": "7",
+    "Digit8": "8",
+    "Digit9": "9",
+    "Digit0": "0",
+    "Minus": "-",
+    "Equal": "=",
+    "Backspace": "Backspace",
+    "Tab": "Tab",
     "KeyQ": "q",
     "KeyW": "w",
     "KeyE": "e",
@@ -196,6 +109,9 @@ let engButtons = {
     "KeyP": "p",
     "BracketLeft": "[",
     "BracketRight": "]",
+    "Backslash": "\\",
+    "Delete": "Del",
+    "CapsLock": "CapsLock",
     "KeyA": "a",
     "KeyS": "s",
     "KeyD": "d",
@@ -207,6 +123,8 @@ let engButtons = {
     "KeyL": "l",
     "Semicolon": ";",
     "Quote": "'",
+    "Enter": "Enter",
+    "ShiftLeft": "Shift",
     "KeyZ": "z",
     "KeyX": "x",
     "KeyC": "c",
@@ -217,9 +135,270 @@ let engButtons = {
     "Comma": ",",
     "Period": ".",
     "Slash": "/",
-};
+    "ArrowUp": "▲",
+    "ShiftRight": "Shift",
+    "ControlLeft": "Ctrl",
+    "MetaLeft": "Win",
+    "AltLeft": "Alt",
+    "Space": " ",
+    "AltRight": "Alt",
+    "ArrowLeft": "◄",
+    "ArrowDown": "▼",
+    "ArrowRight": "►",
+    "ControlRight": "Ctrl"
+    }
+
+let rowsOfButtonsRus = {
+    "Backquote": "ё",
+    "Digit1": "1",
+    "Digit2": "2",
+    "Digit3": "3",
+    "Digit4": "4",
+    "Digit5": "5",
+    "Digit6": "6",
+    "Digit7": "7",
+    "Digit8": "8",
+    "Digit9": "9",
+    "Digit0": "0",
+    "Minus": "-",
+    "Equal": "=",
+    "Backspace": "Backspace",
+    "Tab": "Tab",
+    "KeyQ": "й",
+    "KeyW": "ц",
+    "KeyE": "у",
+    "KeyR": "к",
+    "KeyT": "е",
+    "KeyY": "н",
+    "KeyU": "г",
+    "KeyI": "ш",
+    "KeyO": "щ",
+    "KeyP": "з",
+    "BracketLeft": "х",
+    "BracketRight": "ъ",
+    "Backslash": "\\",
+    "Delete": "Del",
+    "CapsLock": "CapsLock",
+    "KeyA": "ф",
+    "KeyS": "ы",
+    "KeyD": "в",
+    "KeyF": "а",
+    "KeyG": "п",
+    "KeyH": "р",
+    "KeyJ": "о",
+    "KeyK": "л",
+    "KeyL": "д",
+    "Semicolon": "ж",
+    "Quote": "э",
+    "Enter": "Enter",
+    "ShiftLeft": "Shift",
+    "KeyZ": "я",
+    "KeyX": "ч",
+    "KeyC": "с",
+    "KeyV": "м",
+    "KeyB": "и",
+    "KeyN": "т",
+    "KeyM": "ь",
+    "Comma": "б",
+    "Period": "ю",
+    "Slash": ".",
+    "ArrowUp": "▲",
+    "ShiftRight": "Shift",
+    "ControlLeft": "Ctrl",
+    "MetaLeft": "Win",
+    "AltLeft": "Alt",
+    "Space": " ",
+    "AltRight": "Alt",
+    "ArrowLeft": "◄",
+    "ArrowDown": "▼",
+    "ArrowRight": "►",
+    "ControlRight": "Ctrl"
+    }
+
+// let russianButtons = {
+//     "Backquote": "ё",
+//     "KeyQ": "й",
+//     "KeyW": "ц",
+//     "KeyE": "у",
+//     "KeyR": "к",
+//     "KeyT": "е",
+//     "KeyY": "н",
+//     "KeyU": "г",
+//     "KeyI": "ш",
+//     "KeyO": "щ",
+//     "KeyP": "з",
+//     "BracketLeft": "х",
+//     "BracketRight": "ъ",
+//     "KeyA": "ф",
+//     "KeyS": "ы",
+//     "KeyD": "в",
+//     "KeyF": "а",
+//     "KeyG": "п",
+//     "KeyH": "р",
+//     "KeyJ": "о",
+//     "KeyK": "л",
+//     "KeyL": "д",
+//     "Semicolon": "ж",
+//     "Quote": "э",
+//     "KeyZ": "я",
+//     "KeyX": "ч",
+//     "KeyC": "с",
+//     "KeyV": "м",
+//     "KeyB": "и",
+//     "KeyN": "т",
+//     "KeyM": "ь",
+//     "Comma": "б",
+//     "Period": "ю",
+//     "Slash": "."
+// };
+
+// let engButtons = {
+//     "Backquote": "`",
+//     "KeyQ": "q",
+//     "KeyW": "w",
+//     "KeyE": "e",
+//     "KeyR": "r",
+//     "KeyT": "t",
+//     "KeyY": "y",
+//     "KeyU": "u",
+//     "KeyI": "i",
+//     "KeyO": "o",
+//     "KeyP": "p",
+//     "BracketLeft": "[",
+//     "BracketRight": "]",
+//     "KeyA": "a",
+//     "KeyS": "s",
+//     "KeyD": "d",
+//     "KeyF": "f",
+//     "KeyG": "g",
+//     "KeyH": "h",
+//     "KeyJ": "j",
+//     "KeyK": "k",
+//     "KeyL": "l",
+//     "Semicolon": ";",
+//     "Quote": "'",
+//     "KeyZ": "z",
+//     "KeyX": "x",
+//     "KeyC": "c",
+//     "KeyV": "v",
+//     "KeyB": "b",
+//     "KeyN": "n",
+//     "KeyM": "m",
+//     "Comma": ",",
+//     "Period": ".",
+//     "Slash": "/",
+// };
+
+let shiftButtonsRus = {
+    "ShiftLeft": "Shift",
+    "Backquote": "Ё",
+    "Digit1": "!",
+    "Digit2": "\"",
+    "Digit3": "№",
+    "Digit4": ";",
+    "Digit5": "%",
+    "Digit6": ":",
+    "Digit7": "?",
+    "Digit8": "*",
+    "Digit9": "(",
+    "Digit0": ")",
+    "Minus": "_",
+    "Equal": "+",
+    "KeyQ": "Й",
+    "KeyW": "Ц",
+    "KeyE": "У",
+    "KeyR": "К",
+    "KeyT": "Е",
+    "KeyY": "Н",
+    "KeyU": "Г",
+    "KeyI": "Ш",
+    "KeyO": "Щ",
+    "KeyP": "З",
+    "BracketLeft": "Х",
+    "BracketRight": "Ъ",
+    "Backslash": "/",
+    "KeyA": "Ф",
+    "KeyS": "Ы",
+    "KeyD": "В",
+    "KeyF": "А",
+    "KeyG": "П",
+    "KeyH": "Р",
+    "KeyJ": "О",
+    "KeyK": "Л",
+    "KeyL": "Д",
+    "Semicolon": "Ж",
+    "Quote": "Э",
+    "KeyZ": "Я",
+    "KeyX": "Ч",
+    "KeyC": "С",
+    "KeyV": "М",
+    "KeyB": "И",
+    "KeyN": "Т",
+    "KeyM": "Ь",
+    "Comma": "Б",
+    "Period": "Ю",
+    "Slash": ","
+}
+
+let shiftButtonsEng = {
+    "Backquote": "~",
+    "Digit1": "!",
+    "Digit2": "@",
+    "Digit3": "#",
+    "Digit4": "$",
+    "Digit5": "%",
+    "Digit6": "^",
+    "Digit7": "&",
+    "Digit8": "*",
+    "Digit9": "(",
+    "Digit0": ")",
+    "Minus": "_",
+    "Equal": "+",
+    "KeyQ": "Q",
+    "KeyW": "W",
+    "KeyE": "E",
+    "KeyR": "R",
+    "KeyT": "T",
+    "KeyY": "Y",
+    "KeyU": "U",
+    "KeyI": "I",
+    "KeyO": "O",
+    "KeyP": "P",
+    "BracketLeft": "{",
+    "BracketRight": "}",
+    "Backslash": "|",
+    "KeyA": "A",
+    "KeyS": "S",
+    "KeyD": "D",
+    "KeyF": "F",
+    "KeyG": "G",
+    "KeyH": "H",
+    "KeyJ": "J",
+    "KeyK": "K",
+    "KeyL": "L",
+    "Semicolon": ":",
+    "Quote": "\"",
+    "KeyZ": "Z",
+    "KeyX": "X",
+    "KeyC": "C",
+    "KeyV": "V",
+    "KeyB": "B",
+    "KeyN": "N",
+    "KeyM": "M",
+    "Comma": "<",
+    "Period": ">",
+    "Slash": "?"
+}
+
+let checker = `eng`;
+
 
 let body = document.querySelector(`body`);
+
+let title = document.createElement(`p`);
+title.classList.add(`title`);
+title.innerHTML = `RSS Виртуальная клавиатура`;
+body.append(title);
 
 let innerField = document.createElement(`textarea`);
 innerField.classList.add(`textarea`)
@@ -228,6 +407,17 @@ body.append(innerField)
 let keyBoard = document.createElement(`div`);
 keyBoard.classList.add('keyBoard');
 body.append(keyBoard)
+
+let info = document.createElement(`p`);
+info.classList.add(`info`);
+info.innerHTML = `Клавиатура создана в операционной системе Windows`;
+body.append(info);
+
+let lang = document.createElement(`p`);
+lang.classList.add(`lang`);
+lang.innerHTML = `Для переключения языка комбинация: левыe shiftLeft + alt`;
+body.append(lang);
+
 
 
 for (let element of rowsOfButtons) {
@@ -240,27 +430,17 @@ for (let element of rowsOfButtons) {
         button.classList.add(el);
         button.innerHTML = element[el];
         buttonRow.append(button);
-
     }
 }
 
-// document.addEventListener ( `keydown`, function func (e){
-//     for (let element of keyBoard.children) {
-//         for (let el of element.children) {
-//             if ([...el.classList].includes(e.code)) {
-//                 el.style.backgroundColor = `green`;
-//             }
-//         }
-//     }
-// }
-// )
-
 // document.addEventListener(`keydown`, function (e) {
+//     shiftButtonsRus[e.code] = e.key;
 //     console.log(e.code, e.key)
-
+//     console.log(shiftButtonsRus)
 // })
 
 let turner = [];
+
 document.addEventListener(`keydown`, function colorChangerOn (e){
     
     for (let element of keyBoard.children) {
@@ -268,6 +448,7 @@ document.addEventListener(`keydown`, function colorChangerOn (e){
             if ([...el.classList].includes(e.code)) {
                 el.style.transform = `scale(0.9)`;
                 el.style.boxShadow = `0px 10px 20px 2px rgba(0, 0, 0, 0.25)`;
+                el.style.backgroundColor = `#45ff01`;
             }
         }
     }
@@ -280,15 +461,20 @@ document.addEventListener(`keydown`, function colorChangerOn (e){
         turner.splice(0, 1);
     }
     if (turner[0] === 'ShiftLeft' && turner[1] === 'AltLeft')  {
-        for (let element of keyBoard.children) {
-            for (let i = 0; i < element.children.length; i++) { 
-                if (element.children[i].innerHTML != engButtons[element.children[i].classList[1]] && Object.keys(engButtons).includes(element.children[i].classList[1])) {
-                    console.log(engButtons[element.children[i].classList[1]]);
-                    element.children[i].innerHTML = engButtons[element.children[i].classList[1]];
-                } else if (element.children[i].innerHTML != russianButtons[element.children[i].classList[1]] && Object.keys(russianButtons).includes(element.children[i].classList[1])) {
-                    element.children[i].innerHTML = russianButtons[element.children[i].classList[1]];
+        if (checker === `rus`) {
+            for (let element of keyBoard.children) {
+                for (let i = 0; i < element.children.length; i++) { 
+                    element.children[i].innerHTML = rowsOfButtonsEng[element.children[i].classList[1]];
                 }
             }
+            checker = `eng`;
+        } else if (checker === `eng`) {
+            for (let element of keyBoard.children) {
+                for (let i = 0; i < element.children.length; i++) { 
+                    element.children[i].innerHTML = rowsOfButtonsRus[element.children[i].classList[1]];
+                }
+            }
+            checker = `rus`;
         }
 
         console.log(turner)
@@ -309,6 +495,29 @@ document.addEventListener(`keydown`, function colorChangerOn (e){
             }
         }
     }
+
+    // Нормальный рабочий обработчик шифтов на изменение символов!!!!!!!
+
+    if (e.code === 'ShiftLeft' || e.code === `ShiftRight`) {
+        if (checker == `ru`) {
+            for (let element of keyBoard.children) {
+                for (let i = 0; i < element.children.length; i++) { 
+                    if (element.children[i].innerHTML != shiftButtonsRus[element.children[i].classList[1]] && Object.keys(shiftButtonsRus).includes(element.children[i].classList[1])) {
+                        console.log(shiftButtonsRus[element.children[i].classList[1]]);
+                        element.children[i].innerHTML = shiftButtonsRus[element.children[i].classList[1]];
+                    }
+                }
+            }
+        } else if (checker == `en`) {
+            for (let element of keyBoard.children) {
+                for (let i = 0; i < element.children.length; i++) { 
+                    if (element.children[i].innerHTML != shiftButtonsEng[element.children[i].classList[1]] && Object.keys(shiftButtonsEng).includes(element.children[i].classList[1])) {
+                        element.children[i].innerHTML = shiftButtonsEng[element.children[i].classList[1]];
+                    }
+                }
+            }
+        }
+    }
     
 })
 
@@ -318,6 +527,7 @@ document.onkeyup = function colorChangerOff (e){
             if ([...el.classList].includes(e.code)) {
                 el.style.transform = `scale(1)`;
                 el.style.boxShadow = `2px 2px 2px 0px rgba(0,0,0,0.3)`;
+                el.style.backgroundColor = `#0000005e`;
                 if (el.innerHTML.length <= 1) {
                     innerField.innerHTML += el.innerHTML
                 }
@@ -330,20 +540,6 @@ document.onkeyup = function colorChangerOff (e){
 
 //======================================================= Действия Мыши
 
-// for (let element of keyBoard.children) {
-//     for (let el of element.children) {
-//         el.addEventListener(`click`, function func (e) {
-//                 el.style.transform = `scale(0.9)`;
-//                 el.style.boxShadow = `0px 10px 20px 2px rgba(0, 0, 0, 0.25)`;    
-// });
-// el.addEventListener(`click`, function func (e) {
-            
-//     el.style.transform = `scale(1)`;
-//     el.style.boxShadow = `2px 2px 2px 0px rgba(0,0,0,0.3)`;
-
-// })
-//     }
-// }
 
 document.addEventListener(`mousedown`, function func (e) {
     
@@ -353,15 +549,15 @@ document.addEventListener(`mousedown`, function func (e) {
     }
 
     if ([...e.target.classList].includes(`button`) && e.target.innerHTML.length <= 1) {
-        innerField.innerHTML += e.target.innerHTML
+        innerField.innerHTML += e.target.innerHTML;
     }
     
     if ([...e.target.classList].includes(`Enter`)) {
-        innerField.innerHTML += `\n`
+        innerField.innerHTML += `\n`;
     };
 
     if ([...e.target.classList].includes(`Tab`)) {
-        innerField.innerHTML += `   `
+        innerField.innerHTML += `   `;
     }
 
     if ([...e.target.classList].includes(`CapsLock`)) {
@@ -383,7 +579,7 @@ document.addEventListener(`mousedown`, function func (e) {
         innerField.innerHTML = arr.join('');
     }
 
-    if ([...e.target.classList].includes(`Delete`)) {
+    if ([...e.target.classList].includes(`Delete`) && innerField.selectionStart > 0) {
         console.log(innerField.innerHTML.split(``));
         let arr = innerField.innerHTML.split(``);
         arr.splice(innerField.selectionStart, 1);
@@ -392,6 +588,10 @@ document.addEventListener(`mousedown`, function func (e) {
     
 });
 
+innerField.addEventListener(`focus`, function () {
+    
+})
+
 document.addEventListener(`mouseup`, function func (e) {
     if ([...e.target.classList].includes(`button`)) {
         e.target.style.transform = `scale(1)`;
@@ -399,6 +599,3 @@ document.addEventListener(`mouseup`, function func (e) {
     }
 });
 
-innerField.addEventListener(`click`, (e) => {
-    console.log(innerField.selectionStart)
-})
